d2cb9bd9725397e144b2bddfdda8f5c8de41f106
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-11-07 10:26:14 -0600

    added error classes

--- app/app.js
+++ app/app.js
@@ -1,46 +1,66 @@
+// Global variables: bill amount, number of people, all % buttons
 
-// get bill total
-let bill = document.getElementById('bill');
-    
-// get number of people
+const bill = document.getElementById('bill');
 const numOfPeople = document.getElementById('num-people');
-
-// get % of total when button is clicked
-// add % to bill
-// divide (total + percent) = total per person
-// get % of total and divide by number of persons = tip per person
-
 const buttons = document.querySelectorAll('button');
+const headingError = document.querySelector('.num-error');
+
+// Loop over buttons:
+    // get %  when button is clicked
+    // divide total by number of persons = total per person
+    // divide % by number of persons = tip per person
 
 for(let button of buttons) {
     button.addEventListener('click', function() {
+    // button.classList.add('clicked')
+    // button.previousElementSibling.classList.remove('clicked')
+    // button.nextElementSibling.classList.remove('clicked')
 
-        // convert button text percent to decimal
-        const buttonPercent = (parseInt(button.innerText, 10) * 0.01)
-            console.log(buttonPercent)
+    // convert button text to a decimal number
+    const buttonPercent = (parseInt(button.innerText, 10) * 0.01)
+            
+    // convert bill value to a number
+    const billValue = parseInt(bill.value, 10)
+            
+    // multiply buttonPercent by billValue to get total tip amount
+    const totalTip = buttonPercent * billValue
+        
+    // divide totalTip by numOfPeople to get total tip per person
+    const tipPerPerson = (totalTip / numOfPeople.value)
+        
+    // get the total bill per person
+    const totalPerPerson = (billValue / numOfPeople.value)
+           
+    if (numOfPeople.value === '') {
+        numOfPeople.classList.add('error');
+        headingError.style.visibility = 'visible';
+    } else if(numOfPeople.value !== '') {
+        numOfPeople.classList.remove('error');
+        headingError.style.visibility = 'hidden';
+    }
 
-        // convert bill value to a number
-        const billValue = parseInt(bill.value, 10)
-            console.log(billValue)
+    // update the tip amount per person in calculation display
+    document.getElementById('tip-value').innerText = tipPerPerson.toFixed(2)
 
-        // multiply buttonPercent by billValue to get total tip amount
-        const totalTip = buttonPercent * billValue
-        console.log(totalTip)
-
-        // divide totalTip by numOfPeople to get total tip per person
-        const tipPerPerson = (totalTip / numOfPeople.value)
-        console.log(tipPerPerson)
-
-        // get the total bill per person
-        const totalPerPerson = (billValue / numOfPeople.value)
-               
-        // update the tip amount per person calculation display
-        document.getElementById('tip-value').innerText = tipPerPerson.toFixed(2)
-
-        // update the total per person calculation display
-        document.getElementById('total-value').innerText = totalPerPerson.toFixed(2)
+    // update the total per person in calculation display
+    document.getElementById('total-value').innerText = totalPerPerson.toFixed(2)
     })     
 }
+
+// get the value from the custom input
+document.getElementById('custom').addEventListener('input', function() {
+    let customValue = (parseInt(document.getElementById('custom').value, 10) * 0.01)
+
+    const billValue = parseInt(bill.value, 10)
+    const totalTip = customValue * billValue
+    const tipPerPerson = (totalTip / numOfPeople.value)
+    const totalPerPerson = (billValue / numOfPeople.value)
+        
+    document.getElementById('tip-value').innerText = tipPerPerson.toFixed(2)
+    document.getElementById('total-value').innerText = totalPerPerson.toFixed(2)
+})
+
+
 // reset all values and display to 0
 document.getElementById('reset').addEventListener('click', function() {
     document.getElementById('tip-value').innerText = '0.00'
