bac676c09933553cf225af6a5434b1be80837af3
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2021-11-12 08:22:02 -0600

    added Math.trunc

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
+
 // Global variables
+
 const bill = document.getElementById('bill');
 const buttons = document.querySelectorAll('.percent-button');
 const customButton = document.getElementById('custom');
 const numOfPeople = document.getElementById('num-people');
 const headingError = document.querySelector('.num-error');
 const resetButton = document.getElementById('reset');
+const tipDisplay = document.getElementById('tip-value');
+const totalDisplay = document.getElementById('total-value');
 
-// Iterate over buttons:
+// Loop over buttons:
     // get % when button is clicked
     // divide total by number of persons = total per person
     // divide % by number of persons = tip per person
@@ -19,26 +23,33 @@ for (let button of buttons) {
     this.classList.add('clicked');
 
     // convert button text to a decimal number
-    const buttonPercent = (parseInt(button.innerText, 10) * 0.01);
+    const buttonPercent = (parseFloat(button.innerText, 10) * 0.01);
             
     // convert bill value to a number
-    const billValue = parseInt(bill.value, 10);
-            
+    const billValue = parseFloat(bill.value);
+
+    const numPeople = parseInt(numOfPeople.value, 10);
+
     // multiply buttonPercent by billValue to get total tip amount
     const totalTip = buttonPercent * billValue;
         
-    // divide totalTip by numOfPeople to get total tip per person
-    const tipPerPerson = (totalTip / parseInt(numOfPeople.value, 10));
-        
+    /* divide totalTip by numOfPeople to get total tip per person
+    this helped with keeping the decimals from being rounded
+    https://stackoverflow.com/questions/10808671/javascript-how-to-prevent-tofixed-from-rounding-off-decimal-numbers */
+
+    const tipPerPerson = (Math.trunc((totalTip / numPeople)*100)) / 100;
+
     // get the total bill per person
-    const totalPerPerson = (billValue / parseInt(numOfPeople.value, 10));
+    const totalPerPerson = (billValue / numOfPeople.value).toFixed(2);
 
     // Toggling error styling
+
     if (numOfPeople.value === '') {
         numOfPeople.classList.add('error');
         headingError.style.visibility = 'visible';
         reset.style.opacity = '0.35';
-        tipAmount.innerText = '0.00'
+        tipDisplay.innerText = '0.00';
+        totalDisplay.innerText = '0.00';
     } else if (numOfPeople.value !== '') {
         numOfPeople.classList.remove('error');
         headingError.style.visibility = 'hidden';
@@ -47,20 +58,24 @@ for (let button of buttons) {
     }
 
     // update the tip amount per person in calculation display
-    document.getElementById('tip-value').innerText = tipPerPerson.toFixed(2)
+    tipDisplay.innerText = tipPerPerson;
 
     // update the total per person in calculation display
-    document.getElementById('total-value').innerText = totalPerPerson.toFixed(2)
+    totalDisplay.innerText = totalPerPerson;
+
     })     
 }
 
-// get the value from the custom input
-// this helped with adding two event listeners: https://gomakethings.com/listening-to-multiple-events-in-vanilla-js/
+/* get the value from the custom input
+ this helped with adding two event listeners: 
+ https://gomakethings.com/listening-to-multiple-events-in-vanilla-js/ */
+
 const customButtonEvents = function() {
-    const customValue = (parseInt(customButton.value, 10) * 0.01);
-    const billValue = parseInt(bill.value, 10);
+    const customValue = (parseFloat(customButton.value, 10) * 0.01);
+    const billValue = parseFloat(bill.value, 10);
+    const numPeople = parseInt(numOfPeople.value, 10);
     const totalTip = customValue * billValue;
-    const tipPerPerson = (totalTip / numOfPeople.value);
+    const tipPerPerson = (Math.trunc((totalTip / numPeople)*100)) / 100;
     const totalPerPerson = (billValue / numOfPeople.value);
         
     if (numOfPeople.value === '') {
@@ -75,10 +90,9 @@ const customButtonEvents = function() {
         reset.style.cursor = 'pointer';
     }
 
-    document.getElementById('tip-value').innerText = tipPerPerson.toFixed(2)
-    document.getElementById('total-value').innerText = totalPerPerson.toFixed(2)
+    tipDisplay.innerText = tipPerPerson;
+    totalDisplay.innerText = totalPerPerson;
 }
-
 // add event listener, pass in the event type, and call the function
 customButton.addEventListener('input', customButtonEvents);
 customButton.addEventListener('click', customButtonEvents);
